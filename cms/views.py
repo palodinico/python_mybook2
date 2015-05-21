985b60615964aa777266e8b38b0eab38347e54ce
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-21 16:46:40 +0900

    Add a view of book_list

--- cms/views.py
+++ cms/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from cms.models import Book
 
 def book_list(request):
     '''書籍の一覧'''
-    return HttpResponse(u'書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render_to_response('cms/book_list.html',
+                              {'books':books},
+                              context_instance=RequestContext(request))
 
 def book_edit(request, book_id=None):
     '''書籍の編集'''
@@ -12,3 +18,13 @@ def book_edit(request, book_id=None):
 def book_del(request, book_id=None):
     '''書籍の削除'''
     return HttpResponse(u'書籍の削除')
+
+
+'''
+#    return HttpResponse(u'書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render_to_response('cms/book_list.html',  # 使用するテンプレート
+                              {'books': books},       # テンプレートに渡すデータ
+                              context_instance=RequestContext(request))  # その他標準のコンテキスト
+
+'''
