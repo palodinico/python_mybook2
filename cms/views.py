aa2aa41f1080ca5cc58dc9551def88f0e98219a7
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-21 16:01:51 +0900

    Add views in cms/views.py

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
 
-# Create your views here.
+def book_list(request):
+    '''書籍の一覧'''
+    return HttpResponse(u'書籍の一覧')
+
+def book_edit(request, book_id=None):
+    '''書籍の編集'''
+    return HttpResponse(u'書籍の編集')
+
+def book_del(request, book_id=None):
+    '''書籍の削除'''
+    return HttpResponse(u'書籍の削除')
