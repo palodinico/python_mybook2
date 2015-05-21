c4086528050bef42ea65f531b998ddca0062f9d9
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-21 17:22:46 +0900

    Add book_add/_edit funtions

--- cms/views.py
+++ cms/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
+from cms.forms import BookForm
 from cms.models import Book
 
 def book_list(request):
@@ -13,7 +14,22 @@ def book_list(request):
 
 def book_edit(request, book_id=None):
     '''書籍の編集'''
-    return HttpResponse(u'書籍の編集')
+    if book_id:
+        book = get_object_or_404(Book, pk=book_id)
+    else:
+        book = Book()
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:
+        form = BookForm(instance=book)
+
+    return render_to_response('cms/book_edit.html',
+                              dict(form=form, book_id=book_id),
+                              context_instance=RequestContext(request))
 
 def book_del(request, book_id=None):
     '''書籍の削除'''
