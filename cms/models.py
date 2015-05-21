a144e902a7630f83b1acf336c35a8d167e8b5411
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-21 12:58:32 +0900

    Add some models and fields to cms/models.py

--- cms/models.py
+++ cms/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 class Author(models.Model):
     '''著者'''
-    name = models.CharField(u'著者', max_length=255)
+    first_name = models.CharField(u'著者名', max_length=255, default=None)
+    family_name = models.CharField(u'著者性', max_length=255, default=None)
 
     def __str__(self):
-        return self.name
+        return self.family_name + ' ' + self.first_name
 
 class Publisher(models.Model):
     '''出版社'''
