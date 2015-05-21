c568982f8d28f88ec54d99420292546c0b3cbd1b
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-21 12:23:05 +0900

    Add & modify models

--- cms/models.py
+++ cms/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 
+class Author(models.Model):
+    '''著者'''
+    name = models.CharField(u'著者', max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Publisher(models.Model):
+    '''出版社'''
+    name = models.CharField(u'出版社名', max_length=255)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     '''書籍'''
     name = models.CharField(u'書籍名', max_length=255)
-    publisher = models.CharField(u'出版社', max_length=255, blank=True)
     page = models.IntegerField(u'ページ数', blank=True, default = 0)
+    publisher = models.ForeignKey(Publisher, verbose_name=u'出版社', related_name='publishers', blank=True, default=None)
+    author = models.ForeignKey(Author, verbose_name=u'著者', related_name='authors', blank=True, default=None)
 
     def __str__(self):
         return self.name
