b7c5ac2c1d3f7731c4e9d008f765d0c96089e6bc
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-22 17:03:03 +0900

    Update to use BookList

--- cms/urls.py
+++ cms/urls.py
@@ -4,7 +4,7 @@ from cms import views
 
 urlpatterns = patterns('',
     # 書籍
-    url(r'^book/$', views.book_list, name="book_list"),
+    url(r'^book/$', views.BookList.as_view(), name="book_list"),
     url(r'^book/add/$', views.book_edit, name="book_add"),
     url(r'^book/mod/(?P<book_id>\d+)/$', views.book_edit, name="book_mod"),
     url(r'^book/del/(?P<book_id>\d+)/$', views.book_del, name="book_del"),
