c111b4d509548fc3c883b0922ca5f9189492812a
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-22 16:31:03 +0900

    Add views for impression

--- cms/views.py
+++ cms/views.py
@@ -2,8 +2,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
-from cms.forms import BookForm
-from cms.models import Book
+from django.views.generic.list import ListView
+from cms.forms import BookForm, ImreppsionForm
+from cms.models import Book, Impression
 
 def book_list(request):
     '''書籍の一覧'''
@@ -36,3 +37,43 @@ def book_del(request, book_id=None):
     book = get_object_or_404(Book, pk=book_id)
     book.delete()
     return redirect('cms:book_list')
+
+class ImpressionList(ListView):
+    '''感想の一覧'''
+    context_object_name = 'impressions'
+    template_name = 'cms/impression_list.html'
+    paginate_by = 5
+
+    def get(self, request, *args, **kwargs):
+        book = get_object_or_404(Book, pk=kwargs['book_id'])
+        impressions = book.impressions.all().order_by('id')
+        self.object_list = impressions
+        context = self.get_context_data(object_list=self.object_list, book=book)
+        return self.render_to_response(context)
+
+def impression_edit(request, book_id, impression_id=None):
+    '''感想の編集'''
+    book = get_object_or_404(Book, pk=book_id)
+    if impression_id:
+        impression = get_object_or_404(Impression, pk=impression_id)
+    else:
+        impression = Impression()
+
+    if request.method == 'POST':
+        form = ImreppsionForm(request.POST, instance=impression)
+        if form.is_valid():
+            impression = form.save(commit=False)
+            impression.book = book
+            impression.save()
+            return redirect('cms:impression_list', book_id=book_id)
+    else:
+        form = ImreppsionForm(instance=impression)
+    return render_to_response('cms/impression_edit.html',
+                              dict(form=form, book_id=book_id, impression_id=impression_id),
+                              context_instance=RequestContext(request))
+
+def impression_del(request, book_id, impression_id):
+    '''感想の削除'''
+    impression = get_object_or_404(Impression, pk=impression_id)
+    impression.delete()
+    return redirect('cms:impression_list', book_id=book_id)
