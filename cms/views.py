900d537558d6846f6703bdffdcf3c7e514230632
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-22 10:44:46 +0900

    Add book_del function

--- cms/views.py
+++ cms/views.py
@@ -33,14 +33,6 @@ def book_edit(request, book_id=None):
 
 def book_del(request, book_id=None):
     '''書籍の削除'''
-    return HttpResponse(u'書籍の削除')
-
-
-'''
-#    return HttpResponse(u'書籍の一覧')
-    books = Book.objects.all().order_by('id')
-    return render_to_response('cms/book_list.html',  # 使用するテンプレート
-                              {'books': books},       # テンプレートに渡すデータ
-                              context_instance=RequestContext(request))  # その他標準のコンテキスト
-
-'''
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('cms:book_list')
