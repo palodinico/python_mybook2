144077b5c4b5fd8818ccc55787723fdc81c77623
Toshihiro Murata <eb68276@jp.ibm.com>
2015-05-22 16:30:07 +0900

    Add a form to edit an impression

--- cms/forms.py
+++ cms/forms.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.forms import ModelForm
-from cms.models import Book
+from cms.models import Book, Impression
 
 class BookForm(ModelForm):
     '''書籍のフォーム'''
     class Meta:
         model = Book
         fields = ('name', 'publisher', 'author', 'page',)
+
+class ImreppsionForm(ModelForm):
+    '''感想のフォーム'''
+    class Meta:
+        model = Impression
+        fields = ('comment',)
